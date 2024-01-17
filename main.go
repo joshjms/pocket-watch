bd03c212c8b2d50e4e69d6aa5259f1e7d95c3cfb
Clayton Fernalo <clafernalo@gmail.com>
2024-01-17 15:53:20 +0000

    Load environment variables and optional gRPC

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	api "github.com/joshjms/pocket-watch/src"
+	"github.com/joshjms/pocket-watch/src/consts"
 	"github.com/joshjms/pocket-watch/src/isolate"
+	"github.com/joshjms/pocket-watch/src/rpc"
 )
 
 func main() {
+	var err error
+
+	err = consts.Init()
+	if err != nil {
+		errorMsg := fmt.Sprintf("Failed to load environment variable: %v", err)
+		log.Fatal(errorMsg)
+	}
+
 	isolate.InitQueueManager()
-	api.Start()
+	api.StartAPI()
+
+	if os.Getenv("RPC_ENABLE") == "TRUE" {
+		err = rpc.StartServer()
+		if err != nil {
+			panic(err)
+		}
+	}
+
 }
